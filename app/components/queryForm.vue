ac612a17e27483ba751468e5a39eb7ffb5ff188f
cyclictriad <simonmwangi11416@gmail.com>
2025-03-09 13:55:03 +0300

    Changed maxFiles to 500

--- app/components/queryForm.vue
+++ app/components/queryForm.vue
@@ -13,7 +13,7 @@
 
                 <div class="w-full md:w-32">
                     <label for="maxPages" class="block text-sm font-medium text-gray-700 mb-1">Max Pages</label>
-                    <input id="maxPages" v-model.number="searchState.maxPages" type="number" min="1" max="50"
+                    <input id="maxPages" v-model.number="searchState.maxPages" type="number" min="1" max="500"
                         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 </div>
             </div>

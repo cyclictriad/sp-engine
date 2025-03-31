aa6440029030fd183d166818e5c830dc77344231
cyclictriad <simonmwangi11416@gmail.com>
2025-03-31 15:55:05 +0300

    Changed min to 500

--- app/components/queryForm.vue
+++ app/components/queryForm.vue
@@ -44,7 +44,7 @@
                             </div>
                         </div>
                     </div>
-                    <input id="searchDepth" v-model.number="searchState.searchDepth" type="number" min="1" max="50"
+                    <input id="searchDepth" v-model.number="searchState.searchDepth" type="number" min="1" max="500"
                         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 </div>
 
